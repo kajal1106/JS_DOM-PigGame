3d6bfae89c6720e6e1e85c954fd3282079f082fe
Kajal Singh <singh.kajal940@gmail.com>
2020-07-30 15:22:22 +0100

    DOM Event handling : Rolling the dice

--- app.js
+++ app.js
@@ -14,12 +14,15 @@ scores = [0,0];
 roundScore = 0;
 activePlayer = 1;
 
-
-document.querySelector('#current-' + activePlayer).textContent = dice;
+//document.querySelector('#current-' + activePlayer).textContent = dice;
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>'+ dice + '</em>';
 
 document.querySelector('.dice').style.display = 'none';
 
+document.getElementById ('score-0').textContent = '0';
+document.getElementById ('score-1').textContent = '0';
+document.getElementById ('current-0').textContent = '0';
+document.getElementById ('current-1').textContent = '0';
 
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
@@ -28,7 +31,9 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
     var dice =  Math.floor(Math.random() * 6) + 1;
 
     //2. display the result
-    document.querySelector('.dice').style.display = 'block';
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
 
     //3. Update the round score if the rolled number was not a 1
 
